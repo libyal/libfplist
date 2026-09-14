90a0f0226731e003282015d576d904bde844a8c4
Joachim Metz <joachim.metz@gmail.com>
2026-09-14 13:40:56 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libfplist/.libs/libfplist.1.dylib && test -f ./pyfplist/.libs/pyfplist.so
+if [ -f "${PWD}/libfplist/.libs/libfplist.1.dylib" ] && [ -f ./pyfplist/.libs/pyfplist.so ]
 then
-	install_name_tool -change /usr/local/lib/libfplist.1.dylib ${PWD}/libfplist/.libs/libfplist.1.dylib ./pyfplist/.libs/pyfplist.so
+    install_name_tool -change /usr/local/lib/libfplist.1.dylib "${PWD}/libfplist/.libs/libfplist.1.dylib" ./pyfplist/.libs/pyfplist.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
